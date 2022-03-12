c5671fdf8aa0dadbb9bb109f0626f22da893e8e9
voxuantu <19522461@gm.uit.edu.vn>
2022-03-12 19:57:06 +0700

    up tiếp

--- public/javascript/trangChu.js
+++ public/javascript/trangChu.js
@@ -25,3 +25,38 @@ $(function(){
       }
     })
   })
+
+  $(document).ready(function(){
+    $('.js-muon-sach').each(function(i,obj){
+        $(this).click(function(){
+            $.ajax({
+                url:'/api/themSachVaoGio',
+                type:'post',
+                data: {
+                    id: $(this).attr('data-id-book')
+                },
+                success: function(data, status){
+                    if(data.message == "Thanh cong"){
+                        $('#cartQuantity').text(data.cartQuantity)
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Thành công',
+                            text: 'Thêm sách vào giỏ thành công!',
+                        })
+                    }else if(data.message == "That bai"){
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Thất bại',
+                            text: 'Sách này đã có trong giỏ',
+                        })
+                    }else{
+                        window.location.replace('/dangNhap')
+                    }
+                },
+                error : function(err){
+                    console.error(err)
+                }
+            })
+        })
+    })
+})
