2ddd873cf3d8876171f3297a14f361690d002354
vominhtuan1 <19522487@gm.uit.edu.vn>
2022-05-11 08:03:47 +0700

    123

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = new Server(server);
 const muonTraSachController = require('./controllers/staff/muonTraSachController');
 
 var cron = require('node-cron');
-cron.schedule('0 8 * * 0-6', muonTraSachController.sendMailRemind);
+cron.schedule('* * * * *', muonTraSachController.sendMailRemind);
 
 const initializePassport = require('./utils/passport-config')
 initializePassport(passport)
